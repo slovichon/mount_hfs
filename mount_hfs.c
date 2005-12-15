2e445e2c47432ddcd6da61af0bb84cdbe6e46284
yanovich <>
2005-12-15 06:58:01 +0000

    more options

--- mount_hfs.c
+++ mount_hfs.c
@@ -6,22 +6,61 @@
 #include <unistd.h>
 #include "mntopts.h"
 
+__dead void usage(void);
+
 const struct mntopt opts[] = {
 	MOPT_STDOPTS,
 	{ NULL }
 };
-
-__dead void usage(void);
 
 int
 main(int argc, char *argv[])
 {
 	struct hfs_args args;
 	char dir[PATH_MAX];
-	int ch, flags = 0;
+	struct passwd *pwd;
+	struct group *grp;
+	struct stat st;
+	int ch, flags;
+	mode_t mode;
+	uid_t uid;
+	gid_t gid;
+	long l;
 
-	while ((ch = getopt(argc, argv, "")) != -1) {
+	flags = 0;
+	mode = 0;
+	uid = getuid();
+	gid = getgid();
+	while ((ch = getopt(argc, argv, "g:o:u:m:")) != -1) {
 		switch (ch) {
+		case 'g':
+			if ((grp = getgrnam(optarg)) == NULL) {
+				if (errno)
+					err(1, "getgrnam");
+				if ((l = strtol(optarg, NULL, 10)) < 0 ||
+				    l > GID_MAX ||
+				    (grp = getgrgid((gid_t)l)) == NULL)
+					err(1, "%s: unknown group", optarg);
+			}
+			endgrent();
+			break;
+		case 'u':
+			if ((pwd = getpwnam(optarg)) == NULL) {
+				if (errno)
+					err(1, "getpwnam");
+				if ((l = strtol(optarg, NULL, 10)) < 0 ||
+				    l > UID_MAX ||
+				    (pwd = getpwuid((uid_t)l)) == NULL)
+					err(1, "%s: unknown user", optarg);
+			}
+			endpwent();
+			break;
+		case 'm':
+			l = strtol(optarg, NULL, 8);
+			if (((mode_t)l) & ACCESSPERMS)
+				err(1, "%s: invalid mode", optarg);
+			mode = (mode_t)l;
+			break;
 		case 'o':
 			getmntopts(optarg, opts, &flags);
 			break;
@@ -36,6 +75,14 @@ main(int argc, char *argv[])
 	args.fspec = argv[0];
 	if (realpath(argv[1], dir) == -1)
 		err(1, "%s", argv[1]);
+	if (!mode) {
+		if (stat(dir, &st) == NULL)
+			err(1, "%s", dir);
+		mode = st.st_mode & ACCESSPERMS;
+	}
+	args.uid = uid;
+	args.gid = gid;
+	args.mode = mode;
 	if (mount(MOUNT_HFS, dir, flags, &args) == -1)
 		err(1, "mount");
 	exit(0);
@@ -46,7 +93,7 @@ usage(void)
 {
 	extern char *__progname;
 
-	fprintf(stderr, "usage: %s [-o options] special node\n",
-	    __progname);
+	fprintf(stderr, "usage: %s [-g gid] [-m mode] [-o options] "
+	    "[-u uid] special node\n", __progname);
 	exit(1);
 }
